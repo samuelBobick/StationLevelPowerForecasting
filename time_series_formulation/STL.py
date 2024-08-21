b43f541eb40798af0ff7e92f3c09adc0f1451832
Samuel Bobick <samuelbobick@berkeley.edu>
2024-08-21 12:55:38 -0700

    add documentation to time_series_formulation

--- time_series_formulation/STL.py
+++ time_series_formulation/STL.py
@@ -6,34 +6,55 @@ from statsmodels.tsa.api import STLForecast
 from statsmodels.tsa.arima.model import ARIMA
 
 import matplotlib.pyplot as plt
-
 from sklearn.metrics import mean_squared_error
 
 class STLARIMA:
     
-    def __init__(self, lookahead=16, alpha=0.2, period=96, p=1, d=0, q=2):
+    def __init__(self, lookahead=16, num_days_train = 30, alpha=2, period=96, p=1, d=0, q=2):
+        """
+        Args:
+            lookahead (int, optional): How many timesteps ahead we want to predict. Defaults to 16.
+            num_days_train (int, optional): How many days of past data to train model on. Defaults to 30.
+            alpha (float, optional): Underpredictions are penalized alpha times more than overpredictions for weighted error metric. Defaults to 2.
+            period (int, optional): Number of readings per day. Defaults to 96.
+            p (int, optional): ARIMA parameter p. Defaults to 1.
+            d (int, optional): ARIMA parameter d. Defaults to 0.
+            q (int, optional): ARIMA parameter q. Defaults to 2.
+        """
         self.lookahead = lookahead
+        self.num_days_train = num_days_train
         self.alpha = alpha
+        self.period = period
 
         self.p = p
         self.d = d
         self.q = q
 
-        self.period = period
-
-
     def fit(self, train):
+        """Train the model, save it as self.model
+
+        Args:
+            train (DataFrame): Training dataframe train with column "power"
+        """
         power = train['power']
         stlf = STLForecast(power, ARIMA, model_kwargs={"order": (self.p, self.d, self.q)}, period=self.period)
         self.model = stlf.fit()
 
     def predict(self, test):
+        """Given a pandas DataFrame test with a power column, returns error metrics and list of predictions
+
+        Args:
+            test (DataFrame): test DataFrame with column "power"
+
+        Returns:
+            tuple (float, float, list): RMSE, weighted RMSE, list of predictions
+        """
         power = test['power']
 
         all_forecasts = []
         all_actual = []
-        for i in range(30 * self.period, len(power), self.lookahead):
-            train = power[i - 30 * self.period: i - self.lookahead]
+        for i in range(self.num_days_train * self.period, len(power), self.lookahead):
+            train = power[i - self.num_days_train * self.period: i - self.lookahead]
             test = power[i-self.lookahead:i]
 
             stlf = STLForecast(train, ARIMA, model_kwargs={"order": (1, 0, 2)}, period=self.period)
@@ -43,17 +64,12 @@ class STLARIMA:
             all_forecasts.extend(forecast)
             all_actual.extend(test)
 
-
+        # Clip forecasts if they are negative
         all_forecasts = [x if x >= 0 else 0 for x in all_forecasts]
 
-        mse = mean_squared_error(power[30 * self.period : ], all_forecasts)
+        rmse = np.sqrt(mean_squared_error(power[self.num_days_train * self.period : ], all_forecasts))
+        weights = self.alpha ** (1 + np.sign(np.array(all_forecasts) - power[self.num_days_train * self.period : ]))
+        wrmse = np.sqrt(mean_squared_error(forecast, all_forecasts, sample_weight=weights))
 
-        return mse
+        return rmse, wrmse, all_forecasts
     
-# data = pd.read_csv("/Users/sam/Desktop/StationLevelPowerForecasting/time_series_formulation/data.csv")
-# train = data.loc[:71423]
-# test = data.loc[71424:]
-
-# model = STLARIMA()
-# model.fit(train)
-# print(model.predict(test))
