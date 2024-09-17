eeb58b80c8539b454b59a842b60bf81cef658f9b
ThibaudCambronne <thibaud.cambronne@gmail.com>
2024-09-16 17:43:24 -0700

    feat: use window generator in the knn

--- time_series_formulation/main.py
+++ time_series_formulation/main.py
@@ -1,25 +1,23 @@
-import pandas as pd
-import sys
-from typing import Literal
 from collections.abc import Callable
+from typing import Literal
 
-from knn import KNN
 from ffnn import NeuralNet
-from STL import STLARIMA
+from knn import KNN
 from last_week import LastWeek
+from old_knn import OldKNN
 from similar_day import SimilarDay
+from slrp_ev_data import read_old_data, train_test_split
+from STL import STLARIMA
 from visualization import visualize_forecast
 
-from slrp_ev_data import read_old_data
-from slrp_ev_data import train_test_split
-
 model_choice: Literal[
-    "KNN", "Neural Net", "STL with ARIMA", "Last Week", "Similar Day"
-] = "Neural Net"
+    "KNN", "OldKNN", "Neural Net", "STL with ARIMA", "Last Week", "Similar Day"
+] = "KNN"
 
 if __name__ == "__main__":
     dict_model: dict[str, Callable] = {
         "KNN": KNN,
+        "OldKNN": OldKNN,
         "Last Week": LastWeek,
         "Similar Day": SimilarDay,
         "Neural Net": NeuralNet,
@@ -27,18 +25,19 @@ if __name__ == "__main__":
     }
     # Read the data
     data = read_old_data.read_old_data()
-    train, test = train_test_split.train_test_split(data)
+    train, val, test = train_test_split.train_test_split(data, generate_validation=True)  # type: ignore
 
     model = dict_model[model_choice]()
     model.fit(train)
 
-    rmse, wrmse, forecast = model.predict(test)
-    data_lenght_days = len(forecast) // 96
+    df_eval = test
+    rmse, wrmse, forecast, forecast_dates = model.predict(df_eval)
+    data_length_days = len(forecast) // 96
     print(
         f"{model_choice}: ",
         "RMSE: {number:.0f}".format(number=rmse),
         ", WRMSE: {number:.0f}".format(number=wrmse),
-        f"for around {data_lenght_days} days of predictions",
+        f"for around {data_length_days} days of predictions",
     )
 
-    visualize_forecast(test, forecast, 10)
+    visualize_forecast(df_eval, forecast, 10, forecast_dates=forecast_dates)
