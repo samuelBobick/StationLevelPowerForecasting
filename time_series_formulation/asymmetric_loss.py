d680d30168ca8f44cb8347631593ddbfe519f647
ThibaudCambronne <thibaud.cambronne@gmail.com>
2024-10-09 17:37:02 -0700

    feat: add data saving

--- time_series_formulation/asymmetric_loss.py
+++ time_series_formulation/asymmetric_loss.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from default_parameters import BETA
 from sklearn.metrics import root_mean_squared_error  # type: ignore
 
 
@@ -34,4 +35,31 @@ class AsymmetricRMSELoss(nn.Module):
                 torch.pow(self.multiplier, 1 - torch.sign(input - target)) * mse_loss
             )
         )
+        return loss
+
+
+def weighted_peaks_rmse(
+    forecast: np.ndarray | list, real: np.ndarray | list, beta: float = BETA
+) -> float:
+    """
+    Calculate the weighted peaks RMSE. This loss puts more weight on
+    the peaks.
+    """
+    forecast, real = np.array(forecast), np.array(real)
+    # rememnder that the values are scaled and should be between 0 and 1
+    weights = 1 + beta * real
+    # we need to add a 1 to avoid the weight being 0 when real is 0
+    wprmse = root_mean_squared_error(forecast, real, sample_weight=weights)
+    return wprmse
+
+
+class WeightedPeaksRMSELoss(nn.Module):
+    def __init__(self, beta: float = BETA):
+        super(WeightedPeaksRMSELoss, self).__init__()
+        self.beta = beta
+
+    def forward(self, input, target):
+        mse_loss = nn.functional.mse_loss(input, target, reduction="none")
+        weights = 1 + self.beta * target
+        loss = torch.sqrt(torch.mean(weights * mse_loss))
         return loss
--- time_series_formulation/compute_losses.py
+++ time_series_formulation/compute_losses.py
@@ -3,14 +3,22 @@ from typing import TypedDict
 import numpy as np
 import torch
 import torch.nn as nn
-from asymmetric_loss import AsymmetricRMSELoss, asymmetric_rmse
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from asymmetric_loss import (
+    AsymmetricRMSELoss,
+    WeightedPeaksRMSELoss,
+    asymmetric_rmse,
+    weighted_peaks_rmse,
+)
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from torcheval.metrics import R2Score
 
 
 class Losses(TypedDict):
     rmse: float
     wrmse: float
     mae: float
+    wprmse: float
+    r2: float
 
 
 def compute_losses(
@@ -19,7 +27,9 @@ def compute_losses(
     rmse = np.sqrt(mean_squared_error(y_true, y_pred))
     wrmse = asymmetric_rmse(alpha, y_pred, y_true)
     mae = mean_absolute_error(y_true, y_pred)
-    return Losses(rmse=rmse, wrmse=wrmse, mae=mae)  # type: ignore
+    wprmse = weighted_peaks_rmse(y_pred, y_true)
+    r2 = r2_score(y_true, y_pred)
+    return Losses(rmse=rmse, wrmse=wrmse, mae=mae, wprmse=wprmse, r2=r2)  # type: ignore
 
 
 def compute_torch_losses(
@@ -28,18 +38,18 @@ def compute_torch_losses(
     rmse = torch.sqrt(nn.functional.mse_loss(y_pred, y_true, reduction="mean")).item()
     wrmse = AsymmetricRMSELoss(alpha)(y_pred, y_true).item()
     mae = torch.mean(torch.abs(y_true - y_pred)).item()
-    return Losses(rmse=rmse, wrmse=wrmse, mae=mae)
+    wprmse = WeightedPeaksRMSELoss()(y_pred, y_true).item()
+    r2 = R2Score().update(y_pred, y_true).compute().item()
+    return Losses(rmse=rmse, wrmse=wrmse, mae=mae, wprmse=wprmse, r2=r2)
 
 
 def get_real_scale_losses(losses: Losses, normalize_parameters) -> Losses:
     train_min, train_max = normalize_parameters
-    losses["rmse"] = (
-        losses["rmse"] * (train_max["power"] - train_min["power"]) + train_min["power"]
-    )
-    losses["wrmse"] = (
-        losses["wrmse"] * (train_max["power"] - train_min["power"]) + train_min["power"]
-    )
-    losses["mae"] = (
-        losses["mae"] * (train_max["power"] - train_min["power"]) + train_min["power"]
-    )
+    for key in losses:
+        if key not in ["r2"]:
+            losses[key] = (
+                losses[key] * (train_max["power"] - train_min["power"])
+                + train_min["power"]
+            )
+
     return losses
