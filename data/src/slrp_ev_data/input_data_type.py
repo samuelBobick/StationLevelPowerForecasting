cdd8798e0a1851caac647a45274fda2150d4423d
ThibaudCambronne <thibaud.cambronne@gmail.com>
2024-11-12 10:33:53 -0800

    feat: add ucsd data reader

--- data/src/slrp_ev_data/input_data_type.py
+++ data/src/slrp_ev_data/input_data_type.py
@@ -6,7 +6,19 @@ DataSchema = pa.DataFrameSchema(
         # for timezone aware, use: pa.Column(pd.DatetimeTZDtype(unit="ns", tz="America/Los_Angeles")),
         "date": pa.Column(pa.Timestamp),
         "power": pa.Column(
-            pa.Float, pa.Check.greater_than_or_equal_to(0), nullable=True
+            pa.Float,
+            [
+                pa.Check.greater_than_or_equal_to(0),
+                # Make sure that the data is in W (and not kW).
+                # a charger has a max power of 6.6 kW,
+                # so 5_000 W is a reasonable threshold
+                pa.Check(
+                    lambda x: x.max() > 5_000,
+                    element_wise=False,
+                    error="The data does not seem to be in W.",
+                ),
+            ],
+            nullable=True,
         ),
         "workday": pa.Column(pa.Int, nullable=False),
     }
@@ -25,7 +37,7 @@ FeaturedEngineeredSchema = pa.DataFrameSchema(
         ),
         "power": pa.Column(
             pa.Float, nullable=False
-        ),  # , pa.Check.greater_than_or_equal_to(0)
+        ),  # , pa.Check.greater_than_or_equal_to(0) not useable if we standardize
         "time_window": pa.Column(
             pa.Int32, pa.Check.isin(range(6)), nullable=False, required=False
         ),
