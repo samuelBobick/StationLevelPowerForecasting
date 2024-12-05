c7b90af838576fe33c62bbb3e87bccbe06a4ecee
ThibaudCambronne <thibaud.cambronne@gmail.com>
2024-12-05 14:36:16 -0800

    feat: update run_one_model and main to include linear model

--- time_series_formulation/src/slrp_ev_ts_forecasting/main.py
+++ time_series_formulation/src/slrp_ev_ts_forecasting/main.py
@@ -3,22 +3,22 @@ from slrp_ev_ts_forecasting.default_parameters import (
 )
 from slrp_ev_ts_forecasting.run_one_model import run_one_model
 
-model_choices: list[TypeModelChoice] = ["LSTM"]
-number_of_models_per_config = 2
+model_choices: list[TypeModelChoice] = ["LinearRegression"]
+number_of_models_per_config = 1
 
 
 if __name__ == "__main__":
     for model_choice in model_choices:
-        for get_val_data_from_shuffled_train in [False, True]:
+        for get_val_data_from_shuffled_train in [False]:
             for dropout in [0.4]:
                 for batch_norm in [True]:
                     for i in range(number_of_models_per_config):
                         run_one_model(
                             model_choice=model_choice,
                             model_parameters={
-                                "dropout": dropout,
-                                "get_val_data_from_shuffled_train": get_val_data_from_shuffled_train,
-                                "batch_norm": batch_norm,
+                                # "dropout": dropout,
+                                # "get_val_data_from_shuffled_train": get_val_data_from_shuffled_train,
+                                # "batch_norm": batch_norm,
                             },  #
-                            save_results_filename="results_ucsd_val_shuffle",
+                            save_results_filename="results_linear_model",
                         )
