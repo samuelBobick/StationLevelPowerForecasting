64e960fccfa562370aff89e2e0f21775a0e35d40
ThibaudCambronne <thibaud.cambronne@gmail.com>
2025-02-19 14:00:44 -0800

    fix: fix baseline models

--- time_series_formulation/src/slrp_ev_ts_forecasting/models/similar_day.py
+++ time_series_formulation/src/slrp_ev_ts_forecasting/models/similar_day.py
@@ -1,78 +1,103 @@
+import numpy as np
 import pandas as pd
-import slrp_ev_ts_forecasting.default_parameters as default_parameters
-from slrp_ev_ts_forecasting.compute_losses import compute_losses
+from slrp_ev_data.feature_engineering import (
+    convert_date_from_datetime_to_int,
+    convert_date_from_int_to_datetime,
+)
+from slrp_ev_ts_forecasting.models.base import prepare_df_predictions
 
 
 class SimilarDay:
 
-    def __init__(self, alpha=default_parameters.ALPHA, num_days=5, readings_per_day=96):
+    def __init__(
+        self,
+        scaling_mode=None,
+        scaling_parameters=None,
+    ):
         """
         Args:
-            alpha (int, optional): Underpredictions are penalized alpha times more than overpredictions. Defaults to 2.
-            num_days (int, optional): how many past days of the same data/time to average. Defaults to 5.
-            readings_per_day (int, optional): Defaults to 96.
+            scaling_mode (str, optional): UNUSED. Defaults to None.
+            scaling_parameters (dict, optional): UNUSED. Defaults to None.
         """
-        self.alpha = alpha
-        self.num_days = num_days
-        self.readings_per_day = readings_per_day
+        # TODO: we could average on a few days
 
     def predict(self, test: pd.DataFrame):
-        """Given a pandas DataFrame test with a power column, returns error metrics and list of predictions
+        """Given a pandas DataFrame test with a power column, it predicts the power for the next day \
+            using the power of the last similar day (last workday/non-workday).
 
         Args:
-            test (DataFrame):
+            test (DataFrame): test set
 
         Returns:
-            tuple (float, float, list): RMSE, weighted RMSE, list of predictions
+            pd.DataFrame: Dataframe with the predictions
         """
+        test = test.copy()
+        test["date"] = convert_date_from_int_to_datetime(test["date"])
         previous_day_lookup = test.apply(
             self.apply_find_previous_workday, args=(test,), axis=1
         )  # type: ignore
-        first_index_that_can_be_forecasted = (
-            test[previous_day_lookup.isna()].iloc[-1].name + 1
-        )
 
         forecast = []
-        seconds_in_day = 24 * 60 * 60
-        for i in range(first_index_that_can_be_forecasted, test.index[-1] + 1):
-            current_date = test.loc[i, "date"]
+        for i in test.index:
+            day_lookup = previous_day_lookup.loc[i]
+            if pd.isna(day_lookup):
+                forecast.append(None)
+                continue
 
-            forecast.append(
-                test[
-                    test["date"]
-                    == (current_date - seconds_in_day * previous_day_lookup.loc[i])
-                ].iloc[0]["power"]
-            )
+            try:
+                forecast_power = test[test["date"] == day_lookup]["power"].iloc[0]
+            except IndexError:
+                forecast_power = None
 
-        real = test.loc[first_index_that_can_be_forecasted:, "power"]
-        forecast_dates = test.loc[
-            first_index_that_can_be_forecasted:, "date"
-        ].to_numpy()
+            forecast.append(forecast_power)
 
-        losses = compute_losses(forecast, real.to_numpy(), self.alpha)
+        real = test.loc[:, "power"]
+        forecast_dates = convert_date_from_datetime_to_int(
+            test.loc[:, "date"]
+        ).to_numpy()
 
-        return losses, forecast, forecast_dates
+        df_predictions = prepare_df_predictions(
+            np.array(forecast), pd.Series(forecast_dates), real.to_numpy()
+        )
 
-    def fit(self, train):
+        return df_predictions
+
+    @property
+    def model_str_name(self):
+        return "SimilarDay"
+
+    def fit(self, train, val):
+        """Fit method, not used in this model"""
         return
 
     def find_past_similar_day(
-        self, current_day_index: int, df: pd.DataFrame, days_back: int
-    ) -> int | None:
-        is_workday_day_back = (
-            df["workday_0"]
-            .shift(self.readings_per_day * days_back)
-            .loc[current_day_index]
+        self, current_day_index, df: pd.DataFrame, _days_back: int = 1
+    ) -> pd.Timestamp | None:
+        """Recursive function to find the last similar day
+
+        Args:
+            current_day_index: index of the current day
+            df (pd.DataFrame): dataframe with the data
+            _days_back (int, optional): Argument for recursivity, leave to default. how many days back to look for a similar day. Defaults to 1.
+        """
+        day_back_date = df.loc[current_day_index]["date"] - pd.Timedelta(
+            days=_days_back
         )
-        if pd.isna(is_workday_day_back):
+        is_workday_day_back = df.loc[df["date"] == day_back_date]["workday_0"]
+
+        if is_workday_day_back.empty:
+            # TODO: in case of missing data, we should look back an additional day
             return None
-        elif is_workday_day_back == df["workday_0"].loc[current_day_index]:
-            return days_back
         else:
-            return self.find_past_similar_day(current_day_index, df, days_back + 1)
+            is_workday_day_back = is_workday_day_back.iloc[0]
+
+        if is_workday_day_back == df["workday_0"].loc[current_day_index]:
+            return day_back_date
+        else:
+            return self.find_past_similar_day(current_day_index, df, _days_back + 1)
 
     def apply_find_previous_workday(
         self, row: pd.Series, df: pd.DataFrame
-    ) -> int | None:
+    ) -> pd.Timestamp | None:
 
-        return self.find_past_similar_day(row.name, df, days_back=1)
+        return self.find_past_similar_day(row.name, df)
