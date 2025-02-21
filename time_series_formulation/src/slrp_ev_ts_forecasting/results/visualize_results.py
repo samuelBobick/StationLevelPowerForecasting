2e2e5f75eb67b207b3d035da72a33eda7db96800
ThibaudCambronne <thibaud.cambronne@gmail.com>
2025-02-21 11:22:42 -0800

    feat: update results graphs to plotly

--- time_series_formulation/src/slrp_ev_ts_forecasting/results/visualize_results.py
+++ time_series_formulation/src/slrp_ev_ts_forecasting/results/visualize_results.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 from typing import Literal, Optional
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from slrp_ev_ts_forecasting.default_parameters import (
     DEFAULT_RESULTS_FILENAME,
     RESULTS_PATH,
 )
 
-TypeMetrics = Literal["rmse", "wrmse (alpha=2)", "mae", "wprmse (beta=3)", "r2"]
+TypeMetrics = Literal[
+    "rmse", "wrmse (alpha=2)", "mae", "wprmse (beta=3)", "r2", "elapsed_time"
+]
 
 
 def get_groupby_columns(df_results: pd.DataFrame) -> list[str]:
@@ -27,9 +29,14 @@ def get_groupby_columns(df_results: pd.DataFrame) -> list[str]:
             "mae",
             "r2",
             "error_std",
+            "elapsed_time",
         ]
     ]
     return groupby_columns
+
+
+def clean_str(string: str) -> str:
+    return string.replace("_", "\n").capitalize()
 
 
 def visualize_results(
@@ -47,7 +54,6 @@ def visualize_results(
 
     groupby_columns = get_groupby_columns(df_results)
 
-    plt.figure(figsize=(10, 5))
     df_results_grouped = df_results.groupby(groupby_columns)
     df_group_names = pd.DataFrame(
         list(df_results_grouped.indices.keys()), columns=groupby_columns
@@ -65,9 +71,14 @@ def visualize_results(
             df_group["model_name"] += "_" + df_group[column_that_change].astype(str)
             x_axis_title += f" + {column_that_change}"
 
-        ax = sns.boxplot(data=df_group, x="model_name", y=metric_to_show)
+        fig = px.box(
+            df_group,
+            x="model_name",
+            y=metric_to_show,
+            color="model_name",
+            color_discrete_sequence=px.colors.qualitative.__dict__["Plotly"],
+        )
 
-    ax.set_xlabel(x_axis_title)
     # Create a pretty string for the group info
     df_group_index_that_doesnt_change = df_group_names[df_group_names["No Change"]].loc[
         :, 0
@@ -76,23 +87,41 @@ def visualize_results(
         f"{index_name}={df_group_index_that_doesnt_change[index_name]}"
         for index_name in df_group_index_that_doesnt_change.index
     ]
-    # concat all elements of the list info a single string
-    pretty_group = ", ".join(pretty_group)
-
-    plt.title(f"{metric_to_show.upper()} for {pretty_group}")
+    # concat all elements of the list info a single string (and add some line breaks)
+    item_counter = 0
+    pretty_parameters_for_title = ""
+    for item in pretty_group:
+        if item_counter == 4:
+            pretty_parameters_for_title += "<br>"
+            item_counter = 0
+        pretty_parameters_for_title += item + ", "
+        item_counter += 1
+    title = f"{metric_to_show.upper()} for {pretty_parameters_for_title}"
 
     # Modify x-axis labels to be on multiple lines
-    labels = [label.get_text().replace("_", "\n") for label in ax.get_xticklabels()]
-    ax.set_xticklabels(labels)
+    tickvals = df_group["model_name"].unique()
+    ticktext = [label.replace("_", "<br>") for label in tickvals]
 
-    # the model on the x axis are a bit long, so we can add margin to the bottom
-    plt.subplots_adjust(bottom=0.3)
-    plt.show()
+    fig.update_layout(
+        title=title,
+        xaxis_title=x_axis_title,
+        yaxis_title=metric_to_show,
+        legend_title="Model Name",
+        template="plotly_white",
+        width=1300,  # Set the width of the plot
+        height=600,  # Set the height of the plot
+        showlegend=False,  # Show or hide the legend
+        xaxis=dict(tickvals=tickvals, ticktext=ticktext),  # Update x-axis labels
+        margin=dict(l=0, r=0, t=150, b=0),  # Set the margins
+    )
+
+    fig.show()
 
 
 def plot_loss_against_one_parameter(
     metric_to_show: TypeMetrics,
     parameter_to_show: str,
+    second_metric_to_show: Optional[TypeMetrics] = None,
     or_filter_model_name: Optional[list[str]] = None,
     results_file_path: Path = RESULTS_PATH,
     filename: str = DEFAULT_RESULTS_FILENAME,
@@ -124,42 +153,53 @@ def plot_loss_against_one_parameter(
     df_results_grouped = df_results.groupby(groupby_columns)
 
     # Get a list of colors
-    colors = plt.cm.viridis(np.linspace(0, 1, len(df_results_grouped)))  # type: ignore
+    # colors = plt.cm.viridis(np.linspace(0, 1, len(df_results_grouped)))  # type: ignore
+    # colors = pc.qualitative.__dict__["Viridis"]
 
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig = make_subplots(rows=1, cols=1, specs=[[{"secondary_y": True}]])
+
     for i, (group, df_group) in enumerate(df_results_grouped):
         averaged_df_group = (
-            df_group[[metric_to_show, parameter_to_show]]
+            df_group[
+                [metric_to_show, parameter_to_show]
+                + ([second_metric_to_show] if second_metric_to_show else [])
+            ]
             .groupby(parameter_to_show)
             .mean()
         )
-        ax.plot(
-            averaged_df_group.index,
-            averaged_df_group[metric_to_show],
-            label=group[-1],
-            color=colors[i],
+        fig.add_trace(
+            go.Scatter(
+                x=averaged_df_group.index,
+                y=averaged_df_group[metric_to_show],
+                mode="lines+markers" if include_scatter else "lines",
+                name=group[-1] + f" {metric_to_show}",
+            ),
+            secondary_y=False,
         )
-        if include_scatter:
-            ax.scatter(
-                df_group[parameter_to_show],
-                df_group[metric_to_show],
-                color=colors[i],
-                alpha=0.5,
+
+        if second_metric_to_show:
+            fig.add_trace(
+                go.Scatter(
+                    x=averaged_df_group.index,
+                    y=averaged_df_group[second_metric_to_show],
+                    mode="lines+markers" if include_scatter else "lines",
+                    name=group[-1] + f" {second_metric_to_show}",
+                ),
+                secondary_y=True,
             )
 
-    ax.legend()
-    ax.set_xlabel(parameter_to_show)
-    ax.set_ylabel(metric_to_show)
-    if y_limits:
-        if len(y_limits) != 2:
-            raise ValueError(
-                "The y_limits list should be of length 2. The first "
-                "element is the maximum, the second is the minimum "
-                "(e.g. [6000, 7000])"
-            )
-        ax.set_ylim(y_limits)
-    ax.set_title(f"{metric_to_show.upper()} against {parameter_to_show}")
-    plt.show()
+    fig.update_layout(
+        title=f"{metric_to_show} vs {parameter_to_show}",
+        xaxis_title=parameter_to_show,
+        yaxis_title=clean_str(metric_to_show),
+        yaxis2_title=clean_str(second_metric_to_show) if second_metric_to_show else "",
+        legend_title="Model Name",
+        template="plotly_white",
+        width=700,
+        height=400,
+        showlegend=False,
+    )
+    fig.show()
 
 
 def apply_filter(
@@ -181,12 +221,13 @@ def apply_filter(
 
 
 if __name__ == "__main__":
-    plot_loss_against_one_parameter(
-        "rmse",
-        "number_of_artificial_datasets",
-        None,
-        include_scatter=True,
-        filename="experiment_artificial_data_slrpev_new",
-        # y_limits=[5400, 6000],
-    )
-    # visualize_results("rmse", filename="improve_peak_forecasting")
+    # plot_loss_against_one_parameter(
+    #     "rmse",
+    #     "x_dim",
+    #     # second_metric_to_show="elapsed_time",
+    #     or_filter_model_name=["XGBoost"],
+    #     include_scatter=True,
+    #     filename="experiment_lagOpt_202502",
+    #     # y_limits=[5400, 6000],
+    # )
+    visualize_results("rmse", filename="experiment_basic_benchmark_202502")
