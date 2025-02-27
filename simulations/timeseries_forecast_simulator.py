f0cda9aea84f4c81f80024ce2dddbbf1c74b5e89
Samuel Bobick <samuelbobick@berkeley.edu>
2025-02-27 11:13:09 -0800

    fix: get_timeseries_forecast

--- simulations/timeseries_forecast_simulator.py
+++ simulations/timeseries_forecast_simulator.py
@@ -83,12 +83,12 @@ class TimeseriesForecastSimulator(ForecastSimulator):
         )
 
     def get_timeseries(
-        self, row, num_active_sessions, time, verbose=False
+        self, current_row, num_active_sessions, time, verbose=False
     ) -> cp.Expression:
         """Make a forecast for the next self.var_dim_constant timesteps, assuming the new user chooses regular.
 
         Args:
-            row: row of sessions_df that is currently being optimized
+            current_row: row of sessions_df that is currently being optimized
             time (pd.datetime): time of optimization
             num_active_sessions (int): number of active sessions
 
@@ -102,7 +102,7 @@ class TimeseriesForecastSimulator(ForecastSimulator):
             freq="15min",
         )
         next_reg_profile = get_next_reg_profile(
-            row, self.delta_t, self.flexibility_constant, self.power_rate
+            current_row, self.delta_t, self.flexibility_constant, self.power_rate
         )
 
         historical_power_profile = self.aggregate_power_profile[
@@ -153,3 +153,74 @@ class TimeseriesForecastSimulator(ForecastSimulator):
     ):
         # TODO
         pass
+
+    def get_timeseries_forecast(self, current_row, num_active_sessions, time, verbose=False):
+        """Make a forecast for the next self.var_dim_constant timesteps, assuming the new user chooses regular.
+
+        Args:
+            current_row: row of sessions_df that is currently being optimized
+            time (pd.datetime): time of optimization
+            num_active_sessions (int): number of active sessions
+
+        Returns:
+            prediction of current peak given time, past power profile, and scheduled power_profile
+        """
+        rounded_current_time = round_up_to_nearest_timestep(time, self.delta_t)
+        timesteps = pd.date_range(
+            end=rounded_current_time - pd.Timedelta(minutes=15),
+            periods=96,
+            freq="15min",
+        )
+
+        next_reg_profile = get_next_reg_profile(
+            current_row, self.delta_t, self.flexibility_constant, self.power_rate
+        )
+
+        historical_power_profile = self.aggregate_power_profile[
+            self.aggregate_power_profile["date"].isin(timesteps)
+        ]["power"].values
+
+        s_in_day = 24 * 60 * 60  # number of seconds in a day
+        s_in_week = 7 * s_in_day
+        s_in_year = (365.2425) * s_in_day
+        unix_time = time.timestamp()
+        time_features = np.array(
+            [
+                np.sin(unix_time * (2 * np.pi / s_in_day)),
+                np.cos(unix_time * (2 * np.pi / s_in_day)),
+                np.sin(unix_time * (2 * np.pi / s_in_week)),
+                np.cos(unix_time * (2 * np.pi / s_in_week)),
+                np.sin(unix_time * (2 * np.pi / s_in_year)),
+                np.cos(unix_time * (2 * np.pi / s_in_year)),
+            ]
+        )
+
+        time_tomorrow = time + pd.Timedelta(hours=24)
+        tomorrow_workday = int(time_tomorrow.dayofweek < 5)
+        if time_tomorrow.date() in self.holidays.date:
+            tomorrow_workday = 0
+
+        time_in_15_min = time + pd.Timedelta(minutes=15)
+        time_in_15_min_workday = int(time_in_15_min.dayofweek < 5)
+        if time_in_15_min.date() in self.holidays.date:
+            time_in_15_min_workday = 0
+
+        features = cp.hstack(
+            [
+                historical_power_profile * 1000,
+                tomorrow_workday,
+                8, # TODO normalize
+                time_features,
+                (aggregate_future_profile + next_reg_profile) * 1000,
+                num_active_sessions,
+            ]
+        )
+
+        # TODO plug in model from Thibaud and delete my dummy prediction
+        prediction = self.make_prediction(features, time_in_15_min_workday)
+        prediction = np.zeros(self.var_dim_constant)
+
+        if verbose:
+            self.visualize_samples(features.value, prediction.value)  # type: ignore
+
+        return prediction
