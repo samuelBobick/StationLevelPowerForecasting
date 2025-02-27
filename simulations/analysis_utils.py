56741077038ccb4a5b7c69f94be29707d5196cb7
ThibaudCambronne <thibaud.cambronne@gmail.com>
2025-02-26 19:04:26 -0800

    fix: refacto and start fixing final plot

--- simulations/analysis_utils.py
+++ simulations/analysis_utils.py
@@ -2,6 +2,7 @@ import os
 from typing import Literal, Optional
 
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objects as go
 from baseline_simulator import BaselineSimulator
 from constants.tariffs import MODIFIED_DC, TypeTariffName
@@ -156,9 +157,12 @@ def generate_session_results(
         results_file_name (string): filepath to store result dataframe
         summary_file_name (string): filepath of summary dataframe. If summary dataframe doesn't exist, creates a .csv file here.
         aggregate_power_profile_file_name (string): filepath of aggregate_power_profile dataframe. If summary dataframe doesn't exist, creates a .csv file here.
-        verbose (bool, optional): _description_. Defaults to False. If true, prints summary information.
+        verbose (bool, optional): If true, prints summary information of the results. Defaults to False.
+        visualize (bool, optional): If true, visualizes the results. Defaults to False.
     """
-    power_profiles, prices, hourly_prices = sim.simulate()
+    power_profiles, prices, hourly_prices, user_computed_data_for_visualization = (
+        sim.simulate()
+    )
     session_results = get_session_results(
         sim.test_df, power_profiles, prices, sim.power_rate, sim.TOU, sim.delta_t
     )
@@ -212,17 +216,54 @@ def generate_session_results(
         for key in row_data:
             print(f"{key}: {row_data[key]}")
     if visualize:
-        visualize_simulation(sim.aggregate_power_profile)
+        visualize_simulation(
+            sim.aggregate_power_profile,
+            power_profiles,
+            user_computed_data_for_visualization,
+            prices,
+        )
 
 
-def visualize_simulation(aggregate_power_profile: pd.DataFrame) -> None:
+def visualize_simulation(
+    aggregate_power_profile: pd.DataFrame,
+    power_profiles: dict,
+    user_computed_data_for_visualization: dict,
+    prices: dict,
+) -> None:
     """
-    Visualize the results of the simulation values.
+    Visualize the results of the simulation values. the x axis is time
 
     Args:
         aggregate_power_profile: dataframe with columns "date" and "power"
     """
-    fig = go.Figure()
+    df_user_computed_data_for_visualization = pd.DataFrame(
+        user_computed_data_for_visualization
+    ).T
+
+    user_power_profiles_dfs = pd.DataFrame()
+    for user_id, user_power_profile in power_profiles.items():
+        start_charge_time = df_user_computed_data_for_visualization.loc[
+            user_id, "Start charge time"
+        ]
+        date_index = pd.date_range(
+            start=start_charge_time, periods=len(user_power_profile), freq="15min"
+        ).ceil("15min")
+        power_profiles_df = pd.DataFrame(
+            {"date": date_index, "power": user_power_profile}
+        )
+        power_profiles_df["user_id"] = user_id
+        user_power_profiles_dfs = pd.concat(
+            [user_power_profiles_dfs, power_profiles_df]
+        )
+
+    fig = px.area(
+        user_power_profiles_dfs,
+        x="date",
+        y="power",
+        color="user_id",
+        title="Simulated Individual Power Profiles",
+        labels={"power": "Power (kWh)"},
+    )
 
     fig.add_trace(
         go.Scatter(
@@ -233,6 +274,58 @@ def visualize_simulation(aggregate_power_profile: pd.DataFrame) -> None:
         )
     )
 
+    # Add second y-axis for prices
+    fig.update_layout(
+        yaxis2=dict(
+            title="Prices (cents/kWh)",
+            overlaying="y",
+            side="right",
+        )
+    )
+
+    # Add price data to the second y-axis
+    fig.add_trace(
+        go.Scatter(
+            x=df_user_computed_data_for_visualization["Start charge time"],
+            y=[price[0] for user_id, price in prices.items()],
+            mode="lines",
+            name="Scheduled Price",
+            yaxis="y2",
+            line=dict(dash="dash"),
+        ),
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df_user_computed_data_for_visualization["Start charge time"],
+            y=[price[1] for user_id, price in prices.items()],
+            mode="lines",
+            name="Regular Price",
+            yaxis="y2",
+            line=dict(dash="dash"),
+        )
+    )
+
+    # add a dot at the beginning of each session
+    fig.add_trace(
+        go.Scatter(
+            x=df_user_computed_data_for_visualization["Start charge time"],
+            y=[0] * len(df_user_computed_data_for_visualization),
+            mode="markers",
+            marker=dict(
+                size=5,
+            ),
+            showlegend=False,
+            hovertemplate=("{text}"),
+            text=[
+                f"User ID: %{index}<br>"
+                f"Start Charge Time: %{row["Start charge time"]}<br>"
+                f"Energy needed: %{row["Energy needed"]}<br>"
+                f"Duration (hours): %{row["Duration (hours)"]}"
+                for index, row in df_user_computed_data_for_visualization.iterrows()
+            ],
+        )
+    )
     fig.update_layout(
         title=f"Simulated Aggregate Power Profile: {min(aggregate_power_profile['date'].dt.date)} to {max(aggregate_power_profile['date'].dt.date)}",
         xaxis_title="Date",
