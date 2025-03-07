5c68e9e03712969b670240eff75acf223630fd62
Samuel Bobick <samuelbobick@berkeley.edu>
2025-03-06 18:03:57 -0800

    fix: timeseries_simulator overhaul

--- simulations/timeseries_forecast_simulator.py
+++ simulations/timeseries_forecast_simulator.py
@@ -3,14 +3,19 @@ from typing import Optional
 import cvxpy as cp
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 from constants.tariffs import MODIFIED_DC, TypeTariffName
 from forecast_simulator import ForecastSimulator
 from slrp_ev_data.feature_engineering import engineer_time_features
 from utils.utils import (
+    aggregate_u_scheduled_profiles,
     get_aggregate_active_reg_future_profiles,
     get_next_reg_profile,
 )
-from utils.utils_time_and_indexes import round_up_to_nearest_timestep
+from utils.utils_time_and_indexes import (
+    convert_time_to_index,
+    round_up_to_nearest_timestep,
+)
 
 
 class TimeseriesForecastSimulator(ForecastSimulator):
@@ -43,6 +48,40 @@ class TimeseriesForecastSimulator(ForecastSimulator):
             model_name,
         )
 
+        # Caches to save aggregte_future_profile (aka what we know about the future)
+        # and the forecast itself (output of ML algorithm)
+        self.prev_aggregate_u_for_forecast = np.zeros(self.var_dim_constant)
+        self.forecast = np.zeros(self.var_dim_constant)
+
+    def plot_reconstructed_timeseries(self, time, reconstructed_timeseries, forecast):
+        time_forecast = pd.date_range(
+            start=time, periods=len(reconstructed_timeseries), freq=f"{self.delta_t}H"
+        )
+
+        fig = go.Figure()
+
+        fig.add_trace(
+            go.Scatter(
+                x=time_forecast,
+                y=reconstructed_timeseries,
+                mode="lines",
+                name="Reconstructed Timeseries",
+                line=dict(dash="dash", color="red"),
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=time_forecast,
+                y=forecast,
+                mode="lines",
+                name="Forecast",
+                line=dict(dash="dash", color="green"),
+            )
+        )
+
+        fig.show()
+
     def get_current_peak_sch(
         self,
         num_reg_user: int,
@@ -64,22 +103,23 @@ class TimeseriesForecastSimulator(ForecastSimulator):
         Returns:
             cp.Expression: current scheduled peak
         """
-        timeseries = (
-            self.get_timeseries(
-                row, num_reg_user + num_sch_user - 1, time, verbose=verbose
-            )
-            / 1000
-        )
-
         next_reg_profile = get_next_reg_profile(
             row, self.delta_t, self.flexibility_constant, self.power_rate
         )
 
-        return cp.max(
-            timeseries[0]
+        reconstructed_timeseries = (
+            self.forecast
             - next_reg_profile
-            + cp.reshape(u[: self.var_dim_constant], (self.var_dim_constant,))
+            - self.prev_aggregate_u_for_forecast
+            + aggregate_u_scheduled_profiles(u, self.var_dim_constant)
         )
+
+        if verbose:
+            self.plot_reconstructed_timeseries(
+                time, reconstructed_timeseries.value, self.forecast.value
+            )
+
+        return cp.max(reconstructed_timeseries)
 
     def get_current_peak_reg(
         self,
@@ -102,91 +142,52 @@ class TimeseriesForecastSimulator(ForecastSimulator):
         Returns:
             float: current regular peak
         """
-        # Wrap with cp.constant so we can call .value on it
-        return cp.Constant(
-            max(
-                self.get_timeseries(
-                    row, num_reg_user + num_sch_user - 1, time, verbose=verbose
-                )
-                / 1000,
+        # to remove the part that considers the next user as scheduled
+        u_sliced: cp.Variable = u[self.var_dim_constant :]  # type: ignore
+        if u_sliced.shape[0] > 0:
+            u_existing_scheduled = aggregate_u_scheduled_profiles(
+                u_sliced, self.var_dim_constant
             )
+        else:
+            u_existing_scheduled = np.ones(self.var_dim_constant)
+
+        reconstructed_timeseries = (
+            self.forecast - self.prev_aggregate_u_for_forecast + u_existing_scheduled
         )
-
-    def get_timeseries(
-        self, current_row, num_active_sessions, time, verbose=False
-    ) -> cp.Expression:
-        """Make a forecast for the next self.var_dim_constant timesteps, assuming the new user chooses regular.
-
-        Args:
-            current_row: row of sessions_df that is currently being optimized
-            time (pd.datetime): time of optimization
-            num_active_sessions (int): number of active sessions
-
-        Returns:
-            prediction of current peak given time, past power profile, and scheduled power_profile
-        """
-        rounded_current_time = round_up_to_nearest_timestep(time, self.delta_t)
-        timesteps = pd.date_range(
-            end=rounded_current_time - pd.Timedelta(minutes=15),
-            periods=96,
-            freq="15min",
-        )
-        next_reg_profile = get_next_reg_profile(
-            current_row, self.delta_t, self.flexibility_constant, self.power_rate
-        )
-
-        historical_power_profile = self.aggregate_power_profile[
-            self.aggregate_power_profile["date"].isin(timesteps)
-        ]["power"].values
-
-        # get time features
-        time_features = pd.DataFrame(data=[time], columns=["date"])
-        engineer_time_features(time_features)
-        # clean and put in the correct format
-        time_features = time_features.drop(columns=["date"]).values.squeeze()
-        workday = int(time.dayofweek < 5)
-        if time.date() in self.holidays.date:
-            workday = 0
-
-        features = np.hstack(
-            [
-                historical_power_profile * 1000,
-                workday,
-                8,  # number EVSEs available (always 8 in SLRP-EV) TODO standardize?
-                time_features,
-                next_reg_profile * 1000,
-                num_active_sessions,
-            ]
-        )
-
-        prediction = self.make_prediction(features, workday, time)
-        prediction = np.maximum(prediction, 0)
 
         if verbose:
-            self.visualize_samples(time, features, prediction)  # type: ignore
+            self.plot_reconstructed_timeseries(
+                time, reconstructed_timeseries.value, self.forecast.value
+            )
 
-        return prediction
+        return cp.max(reconstructed_timeseries)
 
     def get_timeseries_forecast(
         self,
         current_row,
-        u,
+        u_prev,
         prev_start_charge_time,
+        prev_choice,
         num_active_sessions,
-        time,
+        current_start_charge_time,
         verbose=False,
     ):
         """Make a forecast for the next self.var_dim_constant timesteps, assuming the new user chooses regular.
 
         Args:
             current_row: row of sessions_df that is currently being optimized
+            u: optimal power profile from the previous optimization. We haven't yet done the current optimization
+            prev_start_charge_time: time of previous optimization, aka the start of u_prev
+            prev_choice: previous user choice in the simulation, i.e. REGULAR or SCHEDULED
             time (pd.datetime): time of optimization
             num_active_sessions (int): number of active sessions
 
         Returns:
             prediction of current peak given time, past power profile, and scheduled power_profile
         """
-        rounded_current_time = round_up_to_nearest_timestep(time, self.delta_t)
+        rounded_current_time = round_up_to_nearest_timestep(
+            current_start_charge_time, self.delta_t
+        )
         timesteps = pd.date_range(
             end=rounded_current_time - pd.Timedelta(minutes=15),
             periods=96,
@@ -198,36 +199,42 @@ class TimeseriesForecastSimulator(ForecastSimulator):
         )
 
         if (
-            u is not None
-        ):  # on the first iteration, u will be None - there is no prior session
-            u_sliced = u[self.var_dim_constant :]  # TODO if scheduled, do not slice.
-            if u_sliced.shape[0] > 0:
-                u_reshaped = np.reshape(u_sliced, (u_sliced.shape[0] // 96, 96))
-                u_reshaped = np.sum(u_reshaped, axis=0)
-                timesteps_elapsed = int(
-                    (
-                        pd.to_datetime(current_row["startChargeTime"])
-                        - pd.to_datetime(prev_start_charge_time)
-                    ).total_seconds()
-                    / 3600
-                    // self.delta_t
+            u_prev is not None
+        ):  # on the first iteration, u_prev will be None - there is no prior session
+            if prev_choice == "REGULAR":
+                u_prev_sliced = u_prev[self.var_dim_constant :]
+            else:  # if scheduled, do not slice.
+                u_prev_sliced = u_prev
+
+            if u_prev_sliced.shape[0] > 0:
+                u_prev_reshaped = aggregate_u_scheduled_profiles(
+                    u_prev_sliced, self.var_dim_constant
+                )
+                timesteps_elapsed = convert_time_to_index(
+                    current_start_charge_time - prev_start_charge_time, self.delta_t
                 )
 
                 timesteps_elapsed = min(timesteps_elapsed, self.var_dim_constant)
 
-                u_reshaped = np.pad(
-                    u_reshaped[timesteps_elapsed:],
+                u_prev_reshaped = np.pad(
+                    u_prev_reshaped[timesteps_elapsed:].value,
                     (0, timesteps_elapsed),
                     mode="constant",
                     constant_values=0,
                 )
+            else:
+                u_prev_reshaped = np.zeros(self.var_dim_constant)
 
-                aggregate_future_profile = aggregate_future_profile + u_reshaped
+            self.prev_aggregate_u_for_forecast = u_prev_reshaped
+            aggregate_future_profile = aggregate_future_profile + u_prev_reshaped
 
         aggregate_future_profile = (
             aggregate_future_profile
             + get_aggregate_active_reg_future_profiles(
-                self.test_df, time, self.power_profiles, self.delta_t
+                self.test_df,
+                current_start_charge_time,
+                self.power_profiles,
+                self.delta_t,
             )
         )
 
@@ -236,17 +243,17 @@ class TimeseriesForecastSimulator(ForecastSimulator):
         ]["power"].values
 
         # get time features
-        time_features = pd.DataFrame(data=[time], columns=["date"])
+        time_features = pd.DataFrame(data=[current_start_charge_time], columns=["date"])
         engineer_time_features(time_features)
         # clean and put in the correct format
         time_features = time_features.drop(columns=["date"]).values.squeeze()
 
-        time_tomorrow = time + pd.Timedelta(hours=24)
+        time_tomorrow = current_start_charge_time + pd.Timedelta(hours=24)
         tomorrow_workday = int(time_tomorrow.dayofweek < 5)
         if time_tomorrow.date() in self.holidays.date:
             tomorrow_workday = 0
 
-        time_in_15_min = time + pd.Timedelta(minutes=15)
+        time_in_15_min = current_start_charge_time + pd.Timedelta(minutes=15)
         time_in_15_min_workday = int(time_in_15_min.dayofweek < 5)
         if time_in_15_min.date() in self.holidays.date:
             time_in_15_min_workday = 0
@@ -262,11 +269,12 @@ class TimeseriesForecastSimulator(ForecastSimulator):
             ]
         )
 
-        # TODO plug in model from Thibaud and delete my dummy prediction
-        prediction = self.make_prediction(features, time_in_15_min_workday, time)
-        prediction = np.zeros(self.var_dim_constant)
+        prediction = self.make_prediction(
+            features, time_in_15_min_workday, current_start_charge_time
+        )
 
         if verbose:
-            self.visualize_samples(features.value, prediction.value)  # type: ignore
+            self.visualize_samples(current_start_charge_time, features, prediction.value)  # type: ignore
 
-        return prediction
+        self.aggregate_future_profile_for_forecast = aggregate_future_profile
+        self.forecast = prediction / 1000
